b24ed7a6f9997d48c0024e21691e352c357e7068
juarramdhan <acilrek@gmail.com>
2022-12-04 00:46:30 +0700

    tambah warna

--- lib/app/utils/appColor.dart
+++ lib/app/utils/appColor.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const Color primaryColor = Color.fromRGBO(180, 221, 227, 1);
 const Color secondColor = Color.fromARGB(255, 148, 199, 207);
+const Color seocndText = Color.fromARGB(255, 105, 104, 104);
