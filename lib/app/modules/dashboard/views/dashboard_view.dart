7fc3b1d0d0d0e013881dfa21076b367af72e106b
muhammad-michael-musthofa120 <sielbbx@gmail.com>
2022-12-04 02:09:10 +0700

    menambah bottomnavigation

--- lib/app/modules/dashboard/views/dashboard_view.dart
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -1,3 +1,7 @@
+import 'package:aplikasi_shelter_bima/app/modules/home/views/home_view.dart';
+import 'package:aplikasi_shelter_bima/app/modules/riwayat/views/riwayat_view.dart';
+import 'package:aplikasi_shelter_bima/app/modules/userAccount/views/user_account_view.dart';
+import 'package:aplikasi_shelter_bima/app/utils/appColor.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -8,17 +12,39 @@ class DashboardView extends GetView<DashboardController> {
   const DashboardView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DashboardView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'DashboardView is working',
-          style: TextStyle(fontSize: 20),
+    Get.put(DashboardController());
+    return GetBuilder<DashboardController>(builder: (controller) {
+      return Scaffold(
+        body: SafeArea(
+          child: IndexedStack(
+            index: controller.selectedIndex,
+            children: [
+              HomeView(),
+              UserAccountView(),
+              RiwayatView(),
+            ],
+          ),
         ),
-      ),
-    );
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+            ),
+          ],
+          currentIndex: controller.selectedIndex,
+          selectedItemColor: secondColor,
+          onTap: controller.onItemTapped,
+        ),
+      );
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,7 @@ import 'app/routes/app_pages.dart';
 void main() {
   runApp(
     GetMaterialApp(
-<<<<<<< HEAD
       debugShowCheckedModeBanner: false,
-=======
-    debugShowCheckedModeBanner: false,
->>>>>>> 64c27dfbd3f4d342cc87ce52a2717fbb36d733a0
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
