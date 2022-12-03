773f6748a9f08df641110400bb692256015dfa23
muhammad-michael-musthofa120 <sielbbx@gmail.com>
2022-12-04 03:48:38 +0700

    mendesain homeview

--- lib/app/modules/userAccount/views/user_account_view.dart
+++ lib/app/modules/userAccount/views/user_account_view.dart
@@ -13,109 +13,114 @@ class UserAccountView extends GetView<UserAccountController> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Color.fromARGB(255, 247, 239, 239),
-        body: Container(
-          child: Stack(
-            children: [
-              Container(
-                height: 500,
-                width: 600,
-                decoration: BoxDecoration(
-                  color: primaryColor,
-                ),
-                child: Column(
-                  children: [
-                    SizedBox(height: 150),
-                    Iconify(
-                      Bi.person_circle,
-                      color: Colors.white,
-                      size: 50,
-                    ),
-                    SizedBox(height: 14),
-                    Container(
-                        child: Column(
-                      children: [
-                        Text(
-                          "Juar Ramadhan",
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 105, 104, 104),
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          "JuarRamadhan@gmail.com",
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 105, 104, 104),
+        body: SingleChildScrollView(
+          child: Container(
+            child: Stack(
+              children: [
+                Container(
+                  height: 500,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: primaryColor,
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 150),
+                      Iconify(
+                        Bi.person_circle,
+                        color: Colors.white,
+                        size: 50,
+                      ),
+                      SizedBox(height: 14),
+                      Container(
+                          child: Column(
+                        children: [
+                          Text(
+                            "Juar Ramadhan",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 105, 104, 104),
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold),
                           ),
-                        ),
-                        SizedBox(
-                          height: 100,
-                        ),
-                      ],
-                    )),
-                  ],
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 50, top: 400),
-                height: 200,
-                width: 300,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    color: Color.fromARGB(255, 250, 250, 250)),
-                child: Stack(children: [
-                  Container(
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              left: 20.0, right: 20, top: 40),
-                          child: Container(
-                            child: Column(
-                              children: [
-                                Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Container(
-                                      child: Image.asset(
-                                          'assets/images/profile.jpg',
-                                          height: 120),
-                                    ),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          "Juar Ramadahan",
-                                          style: TextStyle(
-                                              color: Color.fromARGB(
-                                                  255, 105, 104, 104),
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                        Text(
-                                          "Juar@mail.com",
-                                          style: TextStyle(
-                                            color: Color.fromARGB(
-                                                255, 105, 104, 104),
-                                            fontSize: 16,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ],
+                          Text(
+                            "JuarRamadhan@gmail.com",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 105, 104, 104),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  )
-                ]),
-              )
-            ],
+                          SizedBox(
+                            height: 100,
+                          ),
+                        ],
+                      )),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: context.isPhone
+                      ? const EdgeInsets.only(right: 50, left: 50, top: 400)
+                      : const EdgeInsets.only(right: 200, left: 200, top: 400),
+                  height: 200,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Color.fromARGB(255, 250, 250, 250)),
+                  child: Stack(children: [
+                    Container(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 20.0, right: 20, top: 40),
+                            child: Container(
+                              child: Column(
+                                children: [
+                                  Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Container(
+                                        child: Image.asset(
+                                            'assets/images/profile.jpg',
+                                            height: 120),
+                                      ),
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            "Juar Ramadahan",
+                                            style: TextStyle(
+                                                color: Color.fromARGB(
+                                                    255, 105, 104, 104),
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          Text(
+                                            "Juar@mail.com",
+                                            style: TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 105, 104, 104),
+                                              fontSize: 16,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ]),
+                )
+              ],
+            ),
           ),
         ));
   }
